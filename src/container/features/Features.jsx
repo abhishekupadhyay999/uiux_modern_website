516d5cdc914964654b84c7fafea0fc222b952e39
Abhishek Upadhyay <109972672+abhishekupadhyay999@users.noreply.github.com>
2024-06-10 18:32:21 +0530

    first commit

---
+++ src/container/features/Features.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './features.css'
+
+const Features = () => {
+  return (
+    <div className='gpt3__features section__padding' id='features'>
+      <div className='gpt3__feAtures-heading'>
+        <h1 className='gradient__text'>The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h1>
+      </div> 
+      
+    </div>
+  )
+}
+
+export default Features
---
+++ src/container/whatGpt3/WhatGpt3.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './whatGpt3.css'
+import Features from '../features/Features';
+
+const WhatGpt3 = () => {
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+    <div className="gpt3__whatgpt3-feature">
+      
+     
+    </div>
+    <div className="gpt3__whatgpt3-heading">
+      <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+      <p>Explore the Library</p>
+    </div>
+   
+    </div> 
+
+  )
+}
+
+export default WhatGpt3
